5d9ae8a4ac47cededd6c9516199273cbaddb34cc
t4nz456 <t4nz1234@gmail.com>
2023-05-30 16:27:28 +0100

    day2-project

--- Program.cs
+++ Program.cs
@@ -1,18 +1,114 @@
-﻿using System;
+using System;
 
-namespace HelloWorld
+namespace RockPaperScissors
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Contact Information: ");
-            string contactInformation = Console.ReadLine();
-            Console.WriteLine(name + " " + age + " " + contactInformation);
+            int playerScore = 0;
+            int computerScore = 0;
+
+            Console.WriteLine("Welcome to Rock, Paper, Scissors!");
+
+            while (true)
+            {
+                Console.WriteLine("\nSelect your move (rock, paper, scissors) or type 'quit' to end the game:");
+                string playerMove = Console.ReadLine().ToLower();
+
+                if (playerMove == "quit")
+                    break;
+
+                string computerMove = GetComputerMove();
+                Console.WriteLine("Computer chose: " + computerMove);
+
+                string result = DetermineWinner(playerMove, computerMove);
+
+                if (result == "win")
+                {
+                    Console.WriteLine("You win!");
+                    playerScore++;
+                }
+                else if (result == "lose")
+                {
+                    Console.WriteLine("You lose!");
+                    computerScore++;
+                }
+                else
+                {
+                    Console.WriteLine("It's a tie!");
+                }
+
+                Console.WriteLine("Player Score: " + playerScore);
+                Console.WriteLine("Computer Score: " + computerScore);
+            }
+
+            Console.WriteLine("\nFinal Scores:");
+            Console.WriteLine("Player Score: " + playerScore);
+            Console.WriteLine("Computer Score: " + computerScore);
+            Console.WriteLine("Thank you for playing!");
+
+            Console.ReadLine();
+        }
+
+        static string DetermineWinner(string playerMove, string computerMove)
+        {
+            if (playerMove == computerMove)
+                return "tie";
+
+            if (playerMove == "rock")
+            {
+                if (computerMove == "scissors")
+                {
+                    return "win";
+                }
+                else
+                {
+                    return "lose";
+                }
+            }
+
+            if (playerMove == "paper")
+            {
+                if (computerMove == "rock")
+                {
+                    return "win";
+                }
+                else
+                {
+                    return "lose";
+                }
+            }
+            if (playerMove == "scissors")
+            {
+                if (computerMove == "paper")
+                {
+                    return "win";
+                }
+                else
+                {
+                    return "lose";
+                }
+            }
+
+            return "invalid";
+        }
+
+        static string GetComputerMove()
+        {
+            Random random = new Random();
+
+            int randomNumber = random.Next(0, 3);
+
+            switch (randomNumber)
+            {
+                case 0:
+                    return "rock";
+                case 1:
+                    return "paper";
+                default:
+                    return "scissors";
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 Day 1:
 Name Badge Generator: Create a name prompt program that asks users for their name, job, and contact information. Once the program has this information, output a name badge with this information.
 Explore how to accept user input through use of the ReadLine() method.
+
+Day 2:
+Build Something 🔨
+
+Option #1 - Rock, Paper, Scissors: You now know enough to rebuild your Rock, Paper, Scissors project with a few new additions. Create a program that does the following:
+Asks the user for an input of rock, paper, or scissors.
+Randomly selects rock, paper, or scissors for the computer.
+Determines the winner of the game based on the random values that were selected.
+Tracks the player and computers scores.
+Allows the player to play until they opt to end the game.
+Option #2 - Command Line Calculator: Create a C# console application that performs various operations on a given number based on user input.The program should display a menu to the user with the different options like:
+Check if a number is positive or negative.
+Determine if a number is prime.
+Calculate the factorial of a number.
+Exit the program
